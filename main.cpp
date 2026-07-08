d05d91d178ecee10c8cd70562ace301d16d45962
chenxinyu05 <chenxinyu05@chenxinyu05s-MacBook-Pro.local>
2026-07-09 03:50:32 +0800

    fix: add main.cpp

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@
 #include "xxx/xxx_bitset.h"
 
 int main() {
-    xxx_string_t s;
-    xxx_string_init(&s);
-    xxx_string_assign(&s, "abcde", 5);
-    xxx_string_append(&s, xxx_string_c_str(&s), xxx_string_length(&s));
-    printf("%zu %s\n", xxx_string_length(&s), xxx_string_c_str(&s));
-    std::bitset<100> bs;
-    bs.set(1);
-    std::vector<int> v;
-    v.at(1);
+    // xxx_string_t s;
+    // xxx_string_init(&s);
+    // xxx_string_assign(&s, "abcde", 5);
+    // xxx_string_append(&s, xxx_string_c_str(&s), xxx_string_length(&s));
+    // printf("%zu %s\n", xxx_string_length(&s), xxx_string_c_str(&s));
+    // std::bitset<100> bs;
+    // bs.set(1);
+    // std::vector<int> v;
+    // v.at(1);
 }
 
