efe8187448745ce50324db9cd3a2cd6e27e75d7b
chenxinyu05 <chenxinyu05@chenxinyu05s-MacBook-Pro.local>
2026-07-08 20:11:51 +0800

    fix:

--- include/xxx/xxx_bitset.h
+++ include/xxx/xxx_bitset.h
@@ -62,6 +62,7 @@ typedef struct {
     uint64_t words[sizeof(xxx_long_bitset_t) / sizeof(uint64_t)];
 } xxx_short_bitset_t;
 
+#define XXX_SHORT_BITSET_FLAG (1ULL << 63)
 #define XXX_SHORT_BITSET_CAPACITY ((size_t)127)
 
 struct xxx_bitset {
@@ -71,20 +72,11 @@ struct xxx_bitset {
     };
 };
 
-static inline
-size_t xxx_bitset_align(size_t n) {
-#if XXX_BITSET_DEBUG
-    XXX_BITSET_ASSERT(
-        n <= XXX_BITSET_CAPACITY_MAX,
-        "capacity %zu exceeds maximum capacity %zu", n, XXX_BITSET_CAPACITY_MAX);
-#endif
-
-    return (n + 63) & ~(size_t)63;
-}
+#define XXX_BITSET_ALIGN(n) ((n + 63) & ~(size_t)63)
 
 static inline
 bool xxx_bitset_isshort(const xxx_bitset_t *self) {
-    return (self->s.words[1] >> 63) & 1;
+    return (self->s.words[1] & XXX_SHORT_BITSET_FLAG) != 0;
 }
 
 static inline
@@ -95,7 +87,7 @@ int xxx_long_bitset_grow(xxx_bitset_t *self, size_t new_cap) {
         "bitset must be in long representation");
 
     XXX_BITSET_ASSERT(
-        new_cap == xxx_bitset_align(new_cap),
+        new_cap == XXX_BITSET_ALIGN(new_cap),
         "new capacity %zu must be 64-bit aligned", new_cap);
 
     XXX_BITSET_ASSERT(
@@ -107,8 +99,8 @@ int xxx_long_bitset_grow(xxx_bitset_t *self, size_t new_cap) {
         "new capacity %zu exceeds maximum capacity %zu", new_cap, XXX_BITSET_CAPACITY_MAX);
 #endif
 
-    size_t new_nwords = new_cap >> 6;
     size_t old_nwords = self->l.cap >> 6;
+    size_t new_nwords = new_cap >> 6;
     uint64_t *new_words = (uint64_t *)XXX_BITSET_REALLOC(self->l.words, new_nwords * sizeof(uint64_t));
     if (new_words == NULL) {
         return -1;
@@ -127,7 +119,7 @@ int xxx_short_bitset_grow(xxx_bitset_t *self, size_t new_cap) {
         "bitset must be in short representation");
 
     XXX_BITSET_ASSERT(
-        new_cap == xxx_bitset_align(new_cap),
+        new_cap == XXX_BITSET_ALIGN(new_cap),
         "new capacity %zu must be 64-bit aligned", new_cap);
 
     XXX_BITSET_ASSERT(
@@ -145,7 +137,7 @@ int xxx_short_bitset_grow(xxx_bitset_t *self, size_t new_cap) {
         return -1;
     }
     new_words[0] = self->s.words[0];
-    new_words[1] = self->s.words[1] & ~(1ULL << 63);
+    new_words[1] = self->s.words[1] & ~XXX_SHORT_BITSET_FLAG;
     memset(new_words + 2, 0, (nwords - 2) * sizeof(uint64_t));
     self->l.words = new_words;
     self->l.cap = new_cap;
@@ -155,7 +147,7 @@ int xxx_short_bitset_grow(xxx_bitset_t *self, size_t new_cap) {
 static inline
 int xxx_bitset_init(xxx_bitset_t *self) {
     self->s.words[0] = 0;
-    self->s.words[1] = 1ULL << 63;
+    self->s.words[1] = XXX_SHORT_BITSET_FLAG;
     return 0;
 }
 
@@ -178,7 +170,7 @@ int xxx_bitset_copy(xxx_bitset_t *dst, const xxx_bitset_t *src) {
             return 0;
         }
         dst->l.words[0] = src->s.words[0];
-        dst->l.words[1] = src->s.words[1] & ~(1ULL << 63);
+        dst->l.words[1] = src->s.words[1] & ~XXX_SHORT_BITSET_FLAG;
         size_t src_nwords = 2;
         size_t dst_nwords = dst->l.cap >> 6;
         memset(dst->l.words + src_nwords, 0, (dst_nwords - src_nwords) * sizeof(uint64_t));
@@ -226,7 +218,8 @@ size_t xxx_bitset_capacity(const xxx_bitset_t *self) {
 static inline
 size_t xxx_bitset_count(const xxx_bitset_t *self) {
     if (xxx_bitset_isshort(self)) {
-        return __builtin_popcountll(self->s.words[0]) + __builtin_popcountll(self->s.words[1]);
+        return __builtin_popcountll(self->s.words[0])
+            + __builtin_popcountll(self->s.words[1] & ~XXX_SHORT_BITSET_FLAG);
     }
     size_t count = 0;
     size_t nwords = self->l.cap >> 6;
@@ -238,7 +231,16 @@ size_t xxx_bitset_count(const xxx_bitset_t *self) {
 
 static inline
 bool xxx_bitset_empty(const xxx_bitset_t *self) {
-    return xxx_bitset_count(self) == 0;
+    if (xxx_bitset_isshort(self)) {
+        return (self->s.words[0] == 0) && ((self->s.words[1] & ~XXX_SHORT_BITSET_FLAG) == 0);
+    }
+    size_t nwords = self->l.cap >> 6;
+    for (size_t i = 0; i < nwords; ++i) {
+        if (self->l.words[i] != 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 static inline
@@ -246,7 +248,7 @@ int xxx_bitset_reserve(xxx_bitset_t *self, size_t n) {
     if (n > XXX_BITSET_CAPACITY_MAX) {
         return -1;
     }
-    size_t new_cap = xxx_bitset_align(n);
+    size_t new_cap = XXX_BITSET_ALIGN(n);
     if (xxx_bitset_isshort(self)) {
         if (n <= XXX_SHORT_BITSET_CAPACITY) {
             return 0;
@@ -273,7 +275,7 @@ bool xxx_bitset_test(const xxx_bitset_t *self, size_t pos) {
     if (pos >= self->l.cap) {
         return false;
     }
-    return self->l.words[pos >> 6] & (1ULL << (pos & 63));
+    return (self->l.words[pos >> 6] & (1ULL << (pos & 63))) != 0;
 }
 
 static inline
@@ -286,7 +288,7 @@ int xxx_bitset_set(xxx_bitset_t *self, size_t pos) {
             self->s.words[pos >> 6] |= 1ULL << (pos & 63);
             return 0;
         }
-        size_t new_cap = xxx_bitset_align(pos + 1);
+        size_t new_cap = XXX_BITSET_ALIGN(pos + 1);
         if (xxx_short_bitset_grow(self, new_cap) != 0) {
             return -1;
         }
@@ -296,7 +298,7 @@ int xxx_bitset_set(xxx_bitset_t *self, size_t pos) {
     if (pos >= self->l.cap) {
         size_t new_cap = self->l.cap * 2;
         if (new_cap <= pos) {
-            new_cap = xxx_bitset_align(pos + 1);
+            new_cap = XXX_BITSET_ALIGN(pos + 1);
         }
         if (new_cap > XXX_BITSET_CAPACITY_MAX) {
             new_cap = XXX_BITSET_CAPACITY_MAX;
@@ -336,7 +338,7 @@ int xxx_bitset_flip(xxx_bitset_t *self, size_t pos) {
             self->s.words[pos >> 6] ^= 1ULL << (pos & 63);
             return 0;
         }
-        size_t new_cap = xxx_bitset_align(pos + 1);
+        size_t new_cap = XXX_BITSET_ALIGN(pos + 1);
         if (xxx_short_bitset_grow(self, new_cap) != 0) {
             return -1;
         }
@@ -346,7 +348,7 @@ int xxx_bitset_flip(xxx_bitset_t *self, size_t pos) {
     if (pos >= self->l.cap) {
         size_t new_cap = self->l.cap * 2;
         if (new_cap <= pos) {
-            new_cap = xxx_bitset_align(pos + 1);
+            new_cap = XXX_BITSET_ALIGN(pos + 1);
         }
         if (new_cap > XXX_BITSET_CAPACITY_MAX) {
             new_cap = XXX_BITSET_CAPACITY_MAX;
@@ -363,7 +365,7 @@ static inline
 void xxx_bitset_clear(xxx_bitset_t *self) {
     if (xxx_bitset_isshort(self)) {
         self->s.words[0] = 0;
-        self->s.words[1] = 1ULL << 63;
+        self->s.words[1] = XXX_SHORT_BITSET_FLAG;
         return;
     }
     memset(self->l.words, 0, (self->l.cap >> 6) * sizeof(uint64_t));
